8c7577d55b6856bbf78c71766844d8de0f2f3b39
Hector <97549673+hectorherreraespinola@users.noreply.github.com>
2024-12-08 17:36:44 +0000

    update planet

---
+++ Assets/_Scripts/PlanetSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSpawner : MonoBehaviour
+{
+    public GameObject[] planets; // Array de prefabs de planetas
+    public float minInstantiateX = -7f;
+    public float maxInstantiateX = 7f;
+    public float instantiateY = 5.5f;
+    public float spawnInterval = 20f;
+
+    void Start()
+    {
+        InvokeRepeating("InstantiatePlanet", 0f, spawnInterval);
+    }
+
+    void InstantiatePlanet()
+    {
+        Vector3 planetPos = new Vector3(Random.Range(minInstantiateX, maxInstantiateX), instantiateY);
+        int randomIndex = Random.Range(0, planets.Length);
+        Instantiate(planets[randomIndex], planetPos, Quaternion.identity);
+    }
+}
